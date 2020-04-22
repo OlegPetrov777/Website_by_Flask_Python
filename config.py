0f5c070470d48363cd2dc7d56a90dc487cfaed48
Oleg <oleg.ptro737@gmail.com>
2020-04-22 18:28:35 +0300

    First commit

--- config.py
+++ config.py
@@ -2,6 +2,19 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY&') or 'you-will-never-guess&#39'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = 'oleg.ptro737@gmail.com'
+    MAIL_PASSWORD = '143608525Pe'
+    ADMINS = ['your-email@example.com']
+
+    POSTS_PER_PAGE = 5
+
+    LANGUAGES = ['en', 'ru']
+
